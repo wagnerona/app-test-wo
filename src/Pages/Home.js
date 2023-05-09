b2f41c422a338f0aad4cfd6e6b0e3ae78b9bf4c2
wagnerona <wagnerolarco@gmail.com>
2023-05-09 13:39:15 +0100

    doors are responsive

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,21 +1,38 @@
-import React, { useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+// import { Link } from 'react-router-dom';
 import door1 from '../Images/Doors.png';
+import door2 from '../Images/Doors.png';
 
 const Home = () => {
+  const [door1Position, setDoor1Position] = useState('center');
+  const [door2Position, setDoor2Position] = useState('center');
 
+  const handleClick = () => {
+      setDoor1Position('left');
+      setDoor2Position('right');
+  };
 
   return (
     <section name="home" className="bg-pattern h-screen flex items-center justify-center bg-[#FFDDBA] text-white">
-      <div className='w-full'>
+      <div className='w-full h-[28vw] flex overflow-hidden'>
 
+        <img
+          src={door1}
+          alt='Door 1'
+          onClick={() => handleClick()}
+          className={`cursor-pointer door door1 ${door1Position} ml-auto`}
+        />
 
-        
-
+        <img
+          src={door2}
+          alt='Door 2'
+          onClick={() => {handleClick()}}
+          className={`cursor-pointer door door2 ${door2Position} mr-auto`}
+        />
 
       </div>
     </section>
-  )
+  );
 }
 
 
