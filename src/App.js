096e8a43ed7373ab15d6df60253854aea3934646
wagnerona <wagnerolarco@gmail.com>
2023-05-09 10:24:51 +0100

    File Structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./Components/Navbar";
+import './App.css';
+import Home from "./Pages/Home"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+
+
+  return (
+    <section name="home" className="bg-movies h-screen bg-[#C9B6A6] text-white">
+    </section>
+  )
+}
+
+
+export default Home
